893bb3ad7747647a677938b66cc147d9e39265ed
Juan Vasquez <jvasquez@H8243.ad.hugeinc.com>
2018-04-24 17:54:34 -0500

    Adds initial map load with current position

--- src/public/main.js
+++ src/public/main.js
@@ -1,5 +1,6 @@
 
 (function() {
+
     if(!window.foodStore) {
         window.foodStore = {};
         window.foodStore.markers = [];
@@ -24,7 +25,6 @@
         });
 
     function getDataByCurrentLocation() {
-        console.log('looking for locations!');
         if (navigator.geolocation) {
             navigator.geolocation.getCurrentPosition((position) => {
                 getData({
@@ -36,15 +36,13 @@
     }
 
     function getCurrentLocation() {
-        console.log('what???')
         return Rx.Observable.create((observer) => {
             if (navigator.geolocation) {
-                // this first getCurrentPosition is a hack used to make the feature work more consistently
+                // this first getCurrentPosition is a hack used to make geolocation work more consistently
                 navigator.geolocation.getCurrentPosition(function(){}, function(){}, {});
                 // this second getCurrentPosition is the one that is really bringing info
                 navigator.geolocation.getCurrentPosition(position => {
                     observer.next(position);
-                    console.log('next')
                 }, 
                 (error) => console.log('Dud! Something broke down but that\'s Google\'s fault ', error),
                 {enableHighAccuracy:true});
@@ -54,7 +52,7 @@
         });
     }
 
-    const addMarker = (item) => {
+    function addMarker(item) {
         const position = {
             lng: item.venue.location.lng,
             lat: item.venue.location.lat
@@ -68,15 +66,13 @@
         window.foodStore.map.setCenter(position);
     }
 
-    const getData = (coord, radius = 100, category = 'food') => {
-        console.log('getting data ...', coord, radius, category);
+    function getData(coord, radius = 100, category = 'food') {
         const lat = coord.lat;
         const lng = coord.lng;
         const query = `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_KEY}&v=20180422&ll=${lat},${lng}&radius=${radius}&section=${category}`;
         fetch(query)
             .then((resp) => resp.json())
             .then((json) => {
-                // console.log(json);
                 removeMarkers();
                 json.response.groups.forEach((group) => {
                     group.items.map((item) => {
@@ -102,17 +98,21 @@
     }
 
     window.initMap = () => {
-        // var uluru = { lat: 6.1980495, lng: -75.5967791 };
         window.foodStore.map = new google.maps.Map(document.querySelector('.map'), {
-            zoom: 24,
-            // center: uluru
-        });
-        /*
-        var marker = new google.maps.Marker({
-            position: uluru,
-            map: map
-        });
-        */
+            zoom: 24
+          });
+
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(position => {
+                const pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+                };
+                window.foodStore.map.setCenter(pos);
+            });
+        }
+
+        
     }
     
 
