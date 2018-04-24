82d217ca93835ea735f58b9aa5061f0675c92e7f
Juan Vasquez <jvasquez@H8243.ad.hugeinc.com>
2018-04-24 15:03:45 -0500

    Adds Rxjs and radius functionality.  Creates removeMarkers function

--- src/public/main.js
+++ src/public/main.js
@@ -6,8 +6,53 @@
         window.foodStore.map = null;
     }
 
-    const btn = document.querySelector('.btn');
-    btn.addEventListener('click', getDataByLocation);
+    const radius = document.querySelector('.radius');
+
+    Rx.Observable.fromEvent(radius, 'input')
+        .debounceTime(500)
+        .switchMap(
+            () => getCurrentLocation(), 
+            (event, location) => [event, location] 
+        )
+        .subscribe((data) => {
+            const coordinates = {
+                lat: data[1].coords.latitude,
+                lng: data[1].coords.longitude
+            };
+            const radius = data[0].target.value;
+            getData(coordinates, radius); 
+        });
+
+    function getDataByCurrentLocation() {
+        console.log('looking for locations!');
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition((position) => {
+                getData({
+                    lat: position.coords.latitude,
+                    lng: position.coords.longitude
+                })
+            });
+        }
+    }
+
+    function getCurrentLocation() {
+        console.log('what???')
+        return Rx.Observable.create((observer) => {
+            if (navigator.geolocation) {
+                // this first getCurrentPosition is a hack used to make the feature work more consistently
+                navigator.geolocation.getCurrentPosition(function(){}, function(){}, {});
+                // this second getCurrentPosition is the one that is really bringing info
+                navigator.geolocation.getCurrentPosition(position => {
+                    observer.next(position);
+                    console.log('next')
+                }, 
+                (error) => console.log('Dud! Something broke down but that\'s Google\'s fault ', error),
+                {enableHighAccuracy:true});
+            } else {
+                observer.error('You don\'t have support for geolocation');
+            }
+        });
+    }
 
     const addMarker = (item) => {
         const position = {
@@ -23,25 +68,37 @@
         window.foodStore.map.setCenter(position);
     }
 
-    const getData = (coord, radius = 2000, category = 'food') => {
-        console.log('getting data ...');
+    const getData = (coord, radius = 100, category = 'food') => {
+        console.log('getting data ...', coord, radius, category);
         const lat = coord.lat;
         const lng = coord.lng;
         const query = `https://api.foursquare.com/v2/venues/explore?client_id=${CLIENT_ID}&client_secret=${CLIENT_KEY}&v=20180422&ll=${lat},${lng}&radius=${radius}&section=${category}`;
         fetch(query)
             .then((resp) => resp.json())
             .then((json) => {
-                console.log(json);
+                // console.log(json);
+                removeMarkers();
                 json.response.groups.forEach((group) => {
                     group.items.map((item) => {
                         addMarker(item);
                     });
                 });
+
                 const bounds = new google.maps.LatLngBounds();
                 bounds.extend(json.response.suggestedBounds.ne);
                 bounds.extend(json.response.suggestedBounds.sw);
                 window.foodStore.map.fitBounds(bounds);
             });
+    }
+
+    function removeMarkers() {
+        let markers = window.foodStore.markers;
+        if(markers.length > 0) {
+            markers.map((marker) => {
+                marker.setMap(null);
+            })
+        }
+        markers = [];
     }
 
     window.initMap = () => {
@@ -56,20 +113,6 @@
             map: map
         });
         */
-    }
-
-    function getDataByLocation() {
-        console.log('clicked!');
-        btn.setAttribute('disabled', true);
-        if (navigator.geolocation) {
-            navigator.geolocation.getCurrentPosition((position) => {
-                btn.setAttribute('disabled', false);
-                getData({
-                    lat: position.coords.latitude,
-                    lng: position.coords.longitude
-                })
-            });
-        }
     }
     
 
